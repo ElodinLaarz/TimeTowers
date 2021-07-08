90a9ae2d01b5f33dbcb9ea888e65b86e78827d2b
Caleb Geiger <48812321+ElodinLaarz@users.noreply.github.com>
2021-07-08 14:18:54 -0700

    add healthbars

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyStats : MonoBehaviour
 {
-    private int health = 1;
+    private int max_health = 60;
+    private int current_health = 60;
+
+    public RectTransform healthbar;
     
     public void TakeDamage(int damage)
     {
-        health -= damage;
-        if (health <= 0)
+        current_health -= damage;
+        if (current_health <= 0)
         {
             Destroy(gameObject);
             return;
         }
+
+        healthbar.sizeDelta = new Vector2(current_health, healthbar.sizeDelta.y);
     }
 }
